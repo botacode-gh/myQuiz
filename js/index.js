aa5e3ca221dacefcd89b63273f168ab8c7fb79e0
Xaver Schüll <botacode@gmail.com>
2023-03-09 14:03:55 +0100

    Added show/hide answer feature

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 console.clear();
 console.log("It will be ok");
 
-
 // toggle bookmark
 const bookmark = document.querySelector('[data-js="bookmark"]');
 
@@ -13,7 +12,18 @@ bookmark.addEventListener("click", () => {
   toggleBookmark();
 });
 
-
 // toggle answer display
 const answerButton = document.querySelector('[data-js="show-answer-button"]');
-const answer = 
+const answer = document.querySelector('[data-js="answer"]');
+
+function toggleAnswer() {
+  answer.classList.toggle("hidden");
+  if (answerButton.textContent === "Hide answer") {
+    answerButton.textContent = "Show answer";
+  } else {
+    answerButton.textContent = "Hide answer";
+  }
+}
+answerButton.addEventListener("click", () => {
+  toggleAnswer();
+});
